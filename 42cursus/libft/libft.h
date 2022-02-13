ffa78f58d1d6ee15300d82f18dd96a5d368e9896
trimia <marianimatteomaria@gmail.com>
2022-02-13 20:20:39 +0100

    libft_13/02/22_20.20

--- 42cursus/libft/libft.h
+++ 42cursus/libft/libft.h
@@ -6,7 +6,7 @@
 /*   By: mmariani <mmariani@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/01/12 10:56:06 by mmariani          #+#    #+#             */
-/*   Updated: 2022/02/13 20:09:07 by mmariani         ###   ########.fr       */
+/*   Updated: 2022/02/13 20:18:25 by mmariani         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -40,5 +40,5 @@ void	*ft_calloc(size_t count, size_t size);
 char	*ft_strdup(const char *s1);
 char	*ft_substr(char const *s, unsigned int start, size_t len);
 char    *ft_strjoin(char const *s1, char const *s2);
-char	*ft_strtrim(char const *s1, char const *set);
+// char	*ft_strtrim(char const *s1, char const *set);
 #endif
