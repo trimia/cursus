03e53aea27d848ed7af27bdd2854110437a3dc6e
Trimia <trimia@users.noreply.github.com>
2022-02-13 15:05:43 +0100

    libft13/2/22.15.05

--- 42cursus/libft/ft_substr.c
+++ 42cursus/libft/ft_substr.c
@@ -6,11 +6,14 @@
 /*   By: trimia <trimia@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/13 13:24:13 by trimia            #+#    #+#             */
-/*   Updated: 2022/02/13 13:24:19 by trimia           ###   ########.fr       */
+/*   Updated: 2022/02/13 15:03:34 by trimia           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include"libft.h"
 
+char	*ft_substr(char const *s, unsigned int start, size_t len)
+{
 	char	*str;
 	//size_t	*a;
 
