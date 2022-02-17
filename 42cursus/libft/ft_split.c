cfeb14a5bd15fedfd7bd5abc229c7a181072b3f1
trimia <marianimatteomaria@gmail.com>
2022-02-17 13:53:50 +0100

    libft17/02/22

--- 42cursus/libft/ft_split.c
+++ 42cursus/libft/ft_split.c
@@ -6,55 +6,72 @@
 /*   By: mmariani <mmariani@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/16 11:39:29 by mmariani          #+#    #+#             */
-/*   Updated: 2022/02/16 13:24:13 by mmariani         ###   ########.fr       */
+/*   Updated: 2022/02/17 13:02:11 by mmariani         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
+#include<stdio.h>
+
 
 
 static int	ft_strchrboolean( char s, char c)
 {
-		if (s == c)
-			return (0);
-	return (1);
+	if (s == c)
+		return (1);
+	return (0);
+}
+static int ft_count(char *s, char c)
+{
+	size_t	i;
+	while(*s++)
+		if (ft_strchrboolean(*s,c))
+			i++;
+	return (i);
 }
 
+
+char	**ft_rsplit(char const *s, char c, size_t j, size_t start)
+{
+	size_t	i;
+	char	**str;
+	
+	i = 0;
+
+	// k = 0;
+	s--;
+	while (*(++s))
+	{
+		if (ft_strchrboolean(*s,c))
+		{
+			// k++;
+			break;
+		}
+		i++;	
+	}
+	str[j] = ft_substr(a, start, i);
+	j++;
+	start += i;
+	// if (k == ft_count(a, c))
+	// 	return (str);
+	return (ft_rsplit(s, c,j,start));
+}
 
 char	**ft_split(char const *s, char c)
 {
-	size_t	i,j;
-	size_t	start;
-	char	**str;
 	char	*a;
 
-	a=s;
-	i = 0;
-	j = 0;
-	start = 0;
-	while (*s++)
-	{
-		if (ft_strchrboolean(*s,c)==0)
-			break;
-		i++;	
-	}
-	str[j] = ft_substr(a,start,i);
-	j++;
-	ft_split(*s,c);
-	start += i;
-	if (s[i]=='\0')
-		return (str);
-	
-}
+	a=(char *)s;
+	ft_rsplit(a,c,0,0);
 
-#include<stdio.h>
+}
 
 int main ()
 {
 	char **prova;
 	int i =0;
-	prova = ft_split("lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed non risus. Suspendisse", ' ');
-	while (i<=12){
+	prova = ft_split("lorem+ipsum+dolor+sit+amet,+consectetur+adipiscing+elit.+Sed+non+risus.+Suspendisse", '+');
+	while (i<11){
 	printf("%s",prova[i]);
 	i++;
 	}
