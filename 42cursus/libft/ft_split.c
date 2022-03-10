ae139ee4f7415b81d671973aa601dc66d88e6684
trimia <marianimatteomaria@gmail.com>
2022-03-10 20:30:44 +0100

    libft10/03/2220.30

--- 42cursus/libft/ft_split.c
+++ 42cursus/libft/ft_split.c
@@ -6,17 +6,21 @@
 /*   By: mmariani <mmariani@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/16 11:39:29 by mmariani          #+#    #+#             */
-/*   Updated: 2022/03/10 16:35:52 by mmariani         ###   ########.fr       */
+/*   Updated: 2022/03/10 20:21:28 by mmariani         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
-#include <stdio.h>
-static int	ft_strchrboolean(char s, char c)
-{	
-	if (s == c)
-		return (1);
-	return (0);
+
+char	**ft_stilltoomanylines(char **str, char *a)
+{
+	str = malloc(sizeof(char *) * 2);
+	if (!str)
+		return (NULL);
+	str[0] = NULL;
+	str[1] = NULL;
+	free(a);
+	return (str);
 }
 
 static int	ft_count(char *s, char c)
@@ -29,9 +33,9 @@ static int	ft_count(char *s, char c)
 	s--;
 	while (*++s)
 	{
-		if (ft_strchrboolean(*s, c))
+		if (*s == c)
 			i++;
-		while (ft_strchrboolean(*s, c))
+		while (*s == c)
 			s++;
 	}
 	return (i);
@@ -50,7 +54,7 @@ char	**ft_rsplit(char *s, char c, char **str, int x)
 	s--;
 	while (*(++s))
 	{
-		if (ft_strchrboolean(*s, c))
+		if (*s == c)
 		{
 			k++;
 			break ;
@@ -58,21 +62,26 @@ char	**ft_rsplit(char *s, char c, char **str, int x)
 		i++;
 	}
 	*str = ft_substr(a, 0, i);
-	while (ft_strchrboolean(*s, c))
+	while (*s == c)
 			s++;
 	if (!(k == x) && *s)
 		ft_rsplit(s, c, str + 1, k);
 	return (str);
 }
 
-// static char	**ft_toomanylines()
-// {
-// 	char	**str;
-// 	str = (char **)ft_calloc(sizeof(char *), 1);
-// 	if (!str)
-// 		return (NULL);
-// 	return (str);
-// }
+char	**ft_toomanylines(char **str, char *a, char const *s)
+{
+	str = malloc(sizeof(char *) * 2);
+	if (!str)
+		return (NULL);
+	str[0] = malloc(sizeof(char) * (ft_strlen(s) + 1));
+	if (!str[0])
+		return (NULL);
+	str[1] = NULL;
+	ft_strlcpy(str[0], s, ft_strlen(s) + 1);
+	free(a);
+	return (str);
+}
 
 char	**ft_split(char const *s, char c)
 {
@@ -81,56 +90,22 @@ char	**ft_split(char const *s, char c)
 	char	**str;
 	char	sensei[2];
 
+	str = NULL;
 	sensei[0] = c;
 	sensei[1] = 0;
 	a = ft_strtrim(s, (const char *)sensei);
-	if (a == NULL || !s)
-		return (NULL);
-	if (ft_strlen(a) == 0 || c == 0 || c == 48)
-	{
-		str = malloc(sizeof(char *) * 1);
-		str[0] = malloc(sizeof(char) * (ft_strlen(s) + 1));
-		free(a);
-		return (str);
-	}
+	if (s == NULL || a == NULL || ft_strlen(a) == 0)
+		return (ft_stilltoomanylines(str, a));
+	if (ft_strlen(a) == 0 || c == 0)
+		return (ft_toomanylines(str, a, s));
 	x = ft_count(a, c) + 1;
-	str = (char **)malloc(sizeof(char *) * (x));
+	str = (char **)malloc(sizeof(char *) * (x + 1));
 	if (!str)
 		return (NULL);
 	str[x] = 0;
 	while (*s == c)
 		s++;
-	str = ft_rsplit(a, c, str, (x));
+	str = ft_rsplit(a, c, str, (x + 1));
 	free(a);
 	return (str);
-}
-int main ()
-{
-	char const *s1 = "HELLO";
-	char const *s2 = "YOYOY";
-	char *s = "NULLO";
-	unsigned int start = 0;
-	size_t len = 5;
-	int n = 0;
-	char c = 0;
-	char **test;
-	char *yo;
-	printf("__SPLIT__\n");
-	test = ft_split(s1, c);
-	while (test && test[n])
-		free(test[n++]);
-	free(test);
-	// test = ft_split(s1, 0);
-	// while (test && test[n])
-	// 	free(test[n++]);
-	// free(test);
-	// test = ft_split("", c);
-	// while (test && test[n])
-	// 	free(test[n++]);
-	// free(test);
-	// test = ft_split("tripouille", 0);
-	// while (test && test[n])
-	// 	free(test[n++]);
-	// free(test);
-	printf("\033[32mAll good !\n\n\033[37m");
 }
